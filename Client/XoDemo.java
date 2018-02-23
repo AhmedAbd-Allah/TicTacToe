c34ae395d7f8cf557f59ad2bda6cc87a35e19b7a
raniaSayed <raniasayed313@gmail.com>
2018-02-23 22:05:11 +0200

    rename client package

--- Client/XoDemo.java
+++ Client/XoDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xo.demo;
+package TicTacToe.Client;
 
 import java.util.ArrayList;
 import java.util.Scanner;
