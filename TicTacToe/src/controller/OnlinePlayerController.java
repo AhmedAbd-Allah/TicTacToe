d52e907c817c7533e0b46c38668888d1d4e29bfc
aliaa@aliaa-satellite-c850-a826 <aliaasayed45@gmail.com>
2018-03-04 16:54:52 +0200

    playerlist

--- TicTacToe/src/controller/OnlinePlayerController.java
+++ TicTacToe/src/controller/OnlinePlayerController.java
@@ -5,12 +5,17 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +24,8 @@ import javafx.scene.control.Button;
  */
 public class OnlinePlayerController implements Initializable {
     static boolean online_mode=false ;
+    Stage stage;
+    Pane root;
     @FXML
     private Button initiate;
     @FXML
@@ -33,7 +40,11 @@ public class OnlinePlayerController implements Initializable {
     }    
 
     @FXML
-    private void intiateHandler(ActionEvent event) {
+    private void intiateHandler(ActionEvent event) throws IOException {
+        stage = (Stage) initiate.getScene().getWindow();
+        root = (Pane) FXMLLoader.load(getClass().getResource("/views/PlayersList.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
     }
 
     @FXML
